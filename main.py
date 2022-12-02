3d6686989eb37c2b5bf7b80630a344005ba15a69
Mikkel Sørensen <m_hooge@hotmail.com>
2022-12-02 11:09:37 +0100

    Day 2 done plus more scaffolding.

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import argparse
+import importlib
+import util
+
+parser = argparse.ArgumentParser()
+parser.add_argument("task", choices=("run", "init"))
+parser.add_argument("day", type=int)
+parser.add_argument("part", type=int)
+parser.add_argument("-t", "--test", action="store_true")
+
+args = parser.parse_args()
+
+try:
+    if args.task == "init":
+        path = f"code/day_{args.day}.py"
+        if os.path.exists(path):
+            print(f"Error: Python file already exists for day {args.day}. Exiting...")
+            exit(0)
+
+        # Create new .py file
+        print(f"Generating placeholder file at {path}...")
+        with open(path, "w", encoding="utf-8") as fp:
+            fp.write("def part_1(input_text):\n")
+            fp.write("    print(input_text)\n\n")
+            fp.write("def part_2(input_text):\n")
+            fp.write("    print(input_text)\n\n")
+
+        # Download input text from adventofcode
+        print("Downloading input text from adventofcode.com...")
+        util.download_input(args.day)
+
+    else:
+        module = importlib.import_module(f"code.day_{args.day}")
+        input_text = util.read_input(args.day, args.test)
+        getattr(module, f"part_{args.part}")(input_text)
+
+except ModuleNotFoundError:
+    print(f"Error: Can't find code for day {args.day}. Exiting...")
+
+except Exception as exc:
+    print(f"Error: {' - '.join(exc.args)}. Exiting...")
--- util.py
+++ util.py
@@ -20,27 +20,21 @@ def read_input(day, test=False):
 
     else:
         # Use real input
-        path = f"{folder}/day{day}.txt"
-
-        if not os.path.exists(path):
-            # Download input from adventofcode
-            url = f"https://adventofcode.com/2022/day/{day}/input"
-
-            # Get session ID and attach it as a cookie to the request
-            with open("secret.txt", "r", encoding="utf-8") as fp:
-                session_id = fp.readline().strip()
-
-            input_text = requests.get(url, cookies={"session": session_id}).text
-
-            # Write input text to file
-            with open(path, "w", encoding="utf-8") as fp:
-                fp.write(input_text)
-
-            input_text = [line.strip() for line in input_text.split("\n")]
-
-        else:
-            # Load already downloaded input text from file
-            with open(path, "r", encoding="utf-8") as fp:
-                input_text = [line.strip() for line in fp.readlines()]
+        with open(f"{folder}/day{day}.txt", "r", encoding="utf-8") as fp:
+            input_text = [line.strip() for line in fp.readlines()]
 
     return input_text
+
+def download_input(day):
+    # Download input from adventofcode
+    url = f"https://adventofcode.com/2022/day/{day}/input"
+
+    # Get session ID and attach it as a cookie to the request
+    with open("secret.txt", "r", encoding="utf-8") as fp:
+        session_id = fp.readline().strip()
+
+    input_text = requests.get(url, cookies={"session": session_id}).text
+
+    # Write input text to file
+    with open(f"inputs/day{day}.txt", "w", encoding="utf-8") as fp:
+        fp.write(input_text)
